a630a732534408a6e082f114ecaae2e9082f1061
Simon K <jackofspaces@gmail.com>
2024-04-07 17:00:39 +0100

    basic `strings` and `runes`

--- internal/predeclared_types/literals_test.go
+++ internal/predeclared_types/literals_test.go
@@ -7,6 +7,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	zeroInteger int
+	zeroString  string
+	zeroBool    bool
+	zeroFloat   float64
+	zeroRune    rune
+)
+
 func TestLiteralZeroValues(t *testing.T) {
 	assert.Zero(t, zeroInteger)
 	assert.Equal(t, 0.0, zeroFloat)
@@ -14,16 +22,73 @@ func TestLiteralZeroValues(t *testing.T) {
 	assert.Empty(t, zeroString)
 }
 
+const (
+	// This is an example of an interpreted string literal
+	stringLiteralDoubleQuotes = "This is a string literal"
+	// This is an example of a raw string.  It can include
+	// things like \, \n and ""
+	stringLiteralBackTicks = `Raw string \ \n ""`
+)
+
 func TestRawString(t *testing.T) {
 	assert.Contains(t, stringLiteralBackTicks, `\`)
 	assert.Contains(t, stringLiteralBackTicks, `\n`)
 	assert.Contains(t, stringLiteralBackTicks, `""`)
 }
 
+const (
+	// Booleans are bit state
+	isTrue  = true
+	isFalse = false
+)
+
 func TestBooleans(t *testing.T) {
 	assert.True(t, isTrue)
 	assert.False(t, isFalse)
 }
+
+/*
+Go provides both signed and unsigned integers in a variety of predefined sizes.
+Unsigned integers allow only positive numbers but can store higher values due
+to not utilising the first bit to signify the state.
+
+Note: it is considered bad practice to use `u*` based integers everywhere solely
+for the purpose of guaranteeing positive numbers.
+*/
+const (
+	integer8  int8  = 127
+	integer16 int16 = 32767
+	integer32 int32 = 2147483647
+	integer64 int64 = 9223372036854775807
+
+	// Unsigned integers, allow utilising all bits and must be positive only
+	uinteger8  uint8  = 255
+	uinteger16 uint16 = 65535
+	// Adding +1 to the signed counter parts to show it does not
+	// overflow.
+	uintger32  uint32 = 2147483648
+	uinteger64 uint64 = 9223372036854775808
+)
+
+// Go has some special names for some of the integer types.
+// A byte is essentially just an 8 bit unsigned int
+
+const (
+	myByte uint8 = 100
+	// `int` type is not platform agnostic, depending on CPU architecture
+	// it could be a 32 bit or 64 bit integer.
+	myInt int = 99
+
+	// A rune is a 32bit integer (not an unsigned integer like you might expect)
+	// Notice the odd assignment here to a character, it's actually a unicode code
+	// point under the hood.
+	myRune int32 = 'A'
+)
+
+// Which integer type to use?
+// If you are working with binary or network protocols, use integer specific sizes or signs.
+// If you are writing library functions that should work with any int type - use a generic custom function
+// otherwise just use `int`
 
 func TestInt(t *testing.T) {
 	// casting a type of int to 32/64 is compile error
@@ -75,4 +140,47 @@ func TestFloatDifferences(t *testing.T) {
 	_ = f3
 	// Dividing f3 by 0 would return Nan, however no two Nan instances
 	// are ever equal.
+}
+
+// Go has first class support for unicode characters in strings.
+const (
+	// An example of a two-byte unicode code point (U+05E6)
+	containsUnicode = "צ Hi!"
+	// strings are immutable, the following defines a new string
+	concatString = containsUnicode + " world"
+)
+
+// Strings can be compared for equality using == & !=
+// String comparison and ordering can be compared using <, > etc
+// comparison is based on byte by byte analysis, lexicographically
+// shorter strings are padded with null bytes, so a shorter string
+// is ALWAYS lesser than a longer one
+
+func TestStringComparison(t *testing.T) {
+	assert.Equal(t, "foo", "foo")
+	assert.True(t, "foo" != "fo")
+	assert.True(t, "a" < "b")
+	assert.True(t, "z" > "y")
+
+	// Padded null bytes, \x00 is always considered lowest
+	// here ZZZZ has a padded null byte if the common prefix
+	// exists
+	assert.True(t, "AAAAA" < "AA")
+
+	// The above actually compares:
+	// AAAAA < AA\x00\x00\x00
+}
+
+// As touch on previously, a rune (single unicode code point) is represented
+// as an int32 (not uint32)
+const (
+	asciiRune = 'J'
+	// technically true, but don't ever do this
+	asciiJRune int32 = 74
+)
+
+func TestRuneUnicode(t *testing.T) {
+	// the unicode code point for 'J' in the ascii range is 74
+	// again - don't use int32 to represent runes in code, its confusing.
+	assert.True(t, asciiRune == asciiJRune)
 }
