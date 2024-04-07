dda105c168bf40c708f2470a32107ded9e962e1e
Simon K <jackofspaces@gmail.com>
2024-04-07 16:16:31 +0100

    `literals`

--- internal/predeclared_types/literals.go
+++ internal/predeclared_types/literals.go
@@ -15,3 +15,42 @@ const (
 	// things like \, \n and ""
 	stringLiteralBackTicks = `Raw string \ \n ""`
 )
+
+const (
+	// Booleans are bit state
+	isTrue  = true
+	isFalse = false
+)
+
+/*
+Go provides both signed and unsigned integers in a variety of predefined sizes.
+Unsigned integers allow only positive numbers but can store higher values due
+to not utilising the first bit to signify the state.
+
+Note: it is considered bad practice to use `u*` based integers everywhere solely
+for the purpose of guaranteeing positive numbers.
+*/
+const (
+	integer8  int8  = 127
+	integer16 int16 = 32767
+	integer32 int32 = 2147483647
+	integer64 int64 = 9223372036854775807
+
+	// Unsigned integers, allow utilising all bits and must be positive only
+	uinteger8  uint8  = 255
+	uinteger16 uint16 = 65535
+	// Adding +1 to the signed counter parts to show it does not
+	// overflow.
+	uintger32  uint32 = 2147483648
+	uinteger64 uint64 = 9223372036854775808
+)
+
+// Go has some special names for some of the integer types.
+// A byte is essentially just an 8 bit unsigned int
+
+const (
+	myByte uint8 = 100
+	// `int` type is not platform agnostic, depending on CPU architecture
+	// it could be a 32 bit or 64 bit integer.
+	myInt int = 99
+)
--- internal/predeclared_types/literals_test.go
+++ internal/predeclared_types/literals_test.go
@@ -18,3 +18,18 @@ func TestRawString(t *testing.T) {
 	assert.Contains(t, stringLiteralBackTicks, `\n`)
 	assert.Contains(t, stringLiteralBackTicks, `""`)
 }
+
+func TestBooleans(t *testing.T) {
+	assert.True(t, isTrue)
+	assert.False(t, isFalse)
+}
+
+func TestInt(t *testing.T) {
+	// casting a type of int to 32/64 is compile error
+	var my64 int64 = 100000000
+	var my32 int = 10000000
+	_ = my64
+	_ = my32
+	// below would be a compile error, mismatched types
+	// _ = my64 + my32
+}
